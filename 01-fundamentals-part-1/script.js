87a194f61ddbde3fcf161cf227119381b945a1c1
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-06-20 23:48:42 +0100

    add more console.log codes

--- 01-fundamentals-part-1/script.js
+++ 01-fundamentals-part-1/script.js
@@ -1,3 +1,22 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!')
 console.log(40 + 3 + 28 - 10);
+
+console.log('Ehioje');
+console.log(32);
+
+let firstName = 'Ehioje';
+
+console.log(firstName);
+let $function = 27;
+
+let person = 'james';
+let PI = 3.1451;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+// the two variables myFirstJob and myCurrentJob are more descriptive unlike the variables job1 and job2
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob)
