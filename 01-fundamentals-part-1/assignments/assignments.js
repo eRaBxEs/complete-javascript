40a1e5d86636cf02afb7ba7dce1029ad3c336bce
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-06-20 23:49:37 +0100

    add first assignmnt

---
+++ 01-fundamentals-part-1/assignments/assignments.js
@@ -0,0 +1,8 @@
+let country = 'Nigeria';
+let continent = 'Africa';
+let population = 200000000
+
+// log their values to the console
+console.log(country)
+console.log(continent)
+console.log(population)
